f5d1b9a8ecf8ab50185dd8f20a70adb034794b40
vfigunn <vfigun@gmail.com>
2026-02-02 13:06:40 -0300

    Last Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Index from "./pages/Index";
 import Historia from "./pages/Historia";
 import Venues from "./pages/Venues";
@@ -21,6 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <SpeedInsights />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
